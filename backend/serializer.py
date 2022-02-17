fd616b75e1bb5554dc0341abacc8497b30e62746
priteshtagline <77622078+priteshtagline@users.noreply.github.com>
2022-02-17 12:27:51 +0530

    Saved api done

--- backend/serializer.py
+++ backend/serializer.py
@@ -1,5 +1,8 @@
+from bs4 import BeautifulSoup
 from rest_framework import serializers
-from .models.story import Story, Tags
+
+from .models.story import Story
+from .models.story import Tags
 
 
 class TagSerializers(serializers.ModelSerializer):
@@ -16,30 +19,32 @@ class issavedornot(serializers.ModelSerializer):
 
 class storyListSerializers(serializers.ModelSerializer):
     tags = serializers.SerializerMethodField()
-    is_saved = serializers.SerializerMethodField()
 
     class Meta:
         model = Story
         fields = [
             "id",
-            "name",
+            "title",
             "image",
             "content",
+            "tags",
             "create_at",
             "is_read",
-            "tags",
             "is_saved",
         ]
 
     def get_tags(self, obj):
-        return [TagSerializers(s).data for s in obj.tags_set.all()]
+        return [s.name for s in obj.tags_set.all()]
 
-    def get_is_saved(self, obj):
-        if obj.saved.count() >= 1:
-            is_save = True
-        else:
-            is_save = False
-        return is_save
+    def to_representation(self, data):
+        data = super(storyListSerializers, self).to_representation(data)
+        data["content"] = BeautifulSoup(data["content"], "html.parser").get_text()
+        return data
+
+    def to_representation(self, data):
+        data = super(storyListSerializers, self).to_representation(data)
+        data["content"] = BeautifulSoup(data["content"], "html.parser").get_text()
+        return data
 
 
 class storyCreateSerializers(serializers.ModelSerializer):
@@ -47,7 +52,7 @@ class storyCreateSerializers(serializers.ModelSerializer):
         model = Story
         fields = [
             "id",
-            "name",
+            "title",
             "image",
             "content",
             "create_at",
@@ -62,6 +67,28 @@ class isReadSerializers(serializers.ModelSerializer):
 
 
 class savedStorySerializer(serializers.ModelSerializer):
+    is_saved = serializers.SerializerMethodField()
+
     class Meta:
         model = Story
-        fields = ["id", "saved", "name"]
+        fields = [
+            "id",
+            "title",
+            "image",
+            "content",
+            "create_at",
+            "is_read",
+            "is_saved",
+        ]
+
+    def get_is_saved(self, obj):
+        if obj.saved.count() > 0:
+            is_save = True
+        else:
+            is_save = False
+        return is_save
+
+    def to_representation(self, data):
+        data = super(savedStorySerializer, self).to_representation(data)
+        data["content"] = BeautifulSoup(data["content"], "html.parser").get_text()
+        return data
--- backend/urls.py
+++ backend/urls.py
@@ -4,13 +4,17 @@ from .views import *
 urlpatterns = [
     path("publish/<int:pk>/", set_publish_status, name="set-publish-status"),
     # path("api/saved/<int:pk>/", savedStory.as_view(), name=""),
-    path("story/", StorysClass.as_view(), name="storyview"),
+    path("story/", StoryView.as_view(), name="storyview"),
     path("isread/", isReadClass.as_view(), name="isread"),
+    
+    # Save Unsave Story API.
     path(
         "story/<int:pk>/saved/",
         savedStoryClass.as_view(),
         name="saved",
     ),
+    
+    
     path("story/create/", storyCreateClass.as_view(), name="createstory"),
     path("story/<int:pk>", storyRUD.as_view(), name="rudstory"),
 ]
