41bfa2365a42e322be20830eb1dea5db6c357409
Amish2101 <kaklotaramish@gmail.com>
2022-02-21 16:28:08 +0530

    Solve issue in views and serializer

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,6 +2,8 @@ from django.core.mail import send_mail
 from django.dispatch import receiver
 from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
+from django.db.models.signals import post_save, pre_save
+from .models.user import User
 from django.conf import settings
 
 
@@ -24,3 +26,22 @@ def password_reset_token_created(
         # to:
         [reset_password_token.user.email],
     )
+
+
+def update_null_to_empty_string(sender, instance, **kwargs):
+    if instance.email == "null" or instance.email == None:
+        instance.email = ""
+    if instance.first_name == "null" or instance.first_name == None:
+        instance.first_name = ""
+    if instance.last_name == "null" or instance.last_name == None:
+        instance.last_name = ""
+    if instance.phone_number == "null" or instance.phone_number == None:
+        instance.phone_number = ""
+    if instance.device_id == "null" or instance.device_id == None:
+        instance.device_id = ""
+    if instance.device_type == "null" or instance.device_type == None:
+        instance.device_type = ""
+
+
+post_save.connect(update_null_to_empty_string, sender=User)
+pre_save.connect(update_null_to_empty_string, sender=User)
